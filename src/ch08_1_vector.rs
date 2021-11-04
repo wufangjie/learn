4f4f5ea6a363466b183a795b102298b12fb08f01
wfj <wufangjie1223@126.com>
2021-11-04 18:37:27 +0800

    add vec insert and remove

--- src/ch08_1_vector.rs
+++ src/ch08_1_vector.rs
@@ -28,4 +28,19 @@ fn test() {
     // v3.sort_by(|&x: &i32, &y: &i32| x.abs().cmp(&y.abs()));
     assert_eq!(vec![-6, -18, 23, 29, 33, 43, -47, 57, 63, -70, 84], v3);
     // sort_unstable sort_unstable_by
+
+    let mut x = vec![0; 10];
+    assert_eq!(10, x.capacity());
+    let position: Vec<usize> = x.iter().map(|item| item as *const i32 as usize).collect();
+    for i in 1..=9 {
+        x[i] = i as i32;
+    }
+    x.remove(2);
+    assert_eq!(10, x.capacity());
+    assert_eq!(&x[2] as *const i32 as usize, position[2]);
+    assert_eq!(3, x[2]);
+    x.push(10);
+    assert_eq!(10, x.capacity());
+    assert_eq!(3, x.swap_remove(2));
+    assert_eq!(vec![0, 1, 10, 4, 5, 6, 7, 8, 9], x);
 }
